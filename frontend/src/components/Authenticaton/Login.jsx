2077d6477e29a9cb99b3b41079d450beb2ebe8bf
Praveen <praveenpatidar171@gmail.com>
2024-06-03 11:13:56 +0530

    updated

--- frontend/src/components/Authenticaton/Login.jsx
+++ frontend/src/components/Authenticaton/Login.jsx
@@ -31,6 +31,7 @@ export const Login = () => {
         }
         try {
             const config = {
+                credentials: 'include',
                 headers: {
                     'Content-type': 'application/json'
                 },

ac1c034bd2a0b919292b16659d2f296870920050
Praveen <praveenpatidar171@gmail.com>
2024-06-03 13:11:21 +0530

    index.js is updatted

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,21 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mern-chat-app-ivory.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204);
+});
 
 app.use(express.json());
 dotenv.config()
 connectDB();
 const port = process.env.PORT || 5000;
+
+
+
 
 app.use('/api', mainrouter)
 
